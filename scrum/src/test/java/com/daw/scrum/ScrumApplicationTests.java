339a4319fc819bdca0ae7ff14dc37aa09f39f0a5
Noelia Barrionuevo <noe.br97@gmail.com>
2025-12-11 21:10:06 +0100

    creacion de proyecto

---
+++ scrum/src/test/java/com/daw/scrum/ScrumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.scrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
